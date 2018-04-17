0a1fe529be6951bbabd2e4d783dd3f1915d02059
Martin Berthold <martin.berthold@akquinet.de>
2018-04-17 12:48:54 +0200

    change namespace naming

--- src/PemSigner.cs
+++ src/PemSigner.cs
@@ -7,7 +7,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
  */
 #endregion
 
-namespace q2g.HelperPem
+namespace Q2g.HelperPem
 {
     #region Usings
     using Org.BouncyCastle.Crypto;
