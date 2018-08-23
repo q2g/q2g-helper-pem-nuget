33b6ce9cc3cf1d0b17c45e73c599eb19d041d51b
Martin Berthold <martin.berthold@akquinet.de>
2018-08-23 07:26:25 +0200

    Change .Net Core Framework to 2.1

--- src/PemSigner.cs
+++ src/PemSigner.cs
@@ -11,9 +11,7 @@ namespace Q2g.HelperPem
 {
     #region Usings
     using Org.BouncyCastle.Crypto;
-    using Org.BouncyCastle.Crypto.Generators;
     using Org.BouncyCastle.Crypto.Parameters;
-    using Org.BouncyCastle.Crypto.Prng;
     using Org.BouncyCastle.OpenSsl;
     using Org.BouncyCastle.Security;
     using System;
